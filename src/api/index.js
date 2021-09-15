fe32b422f5aab1458203ebdd2ea5ae235d430837
linyoujie <linyoujie@bytedance.com>
2021-09-15 18:06:06 -0400

    db connection

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  // baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: 'http://localhost:3306',
 });
 
 export default api;
